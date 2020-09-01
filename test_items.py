4b03d011571d482aea832121438dcccc7073a062
belayakaryna <69214223+belayakaryna@users.noreply.github.com>
2020-09-01 14:11:33 +0300

    Delete test_items.py

--- test_items.py
+++
@@ -1,8 +0,0 @@
-import pytest
-import time
-from selenium import webdriver
-link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
-def test_dz(browser):
-    browser.get(link)
-    time.sleep(30)
-    assert browser.find_element_by_css_selector("[class='btn btn-lg btn-primary btn-add-to-basket']")
